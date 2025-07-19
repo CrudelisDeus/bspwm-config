e706e6a990a55485812e7ff937f4c5703a98c01d
dmytro <foxi.deus@gmail.com>
2025-07-19 19:22:36 +0300

    init script

---
+++ arch-install.sh
@@ -0,0 +1,23 @@
+# Install latest update
+
+sudo pacman -Syu
+
+# [1] Xorg
+
+sudo pacman -S xorg-server xorg-xinit --noconfirm
+
+# [2] Fonts
+
+sudo pacman -S gnu-free-fonts --noconfirm
+
+# [3] Install videodriver
+
+sudo pacman -S xf86-video-vesa --noconfirm
+
+# [4] BSPWM and related packages
+
+sudo pacman -S bspwm sxhkd dmenu polybar picom --noconfirm
+
+# [5] Terminal - Kitty install
+
+sudo pacman -S kitty
